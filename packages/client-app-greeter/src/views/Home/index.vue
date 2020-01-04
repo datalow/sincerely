f3bdcb5bb60c8edf4d751e693a7a5a72b1efdda5
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2020-01-05 03:47:09 +0400

    fix: fetch greeting from server
    Make use of Vuex store state and actions
    
    Also use pug instead of html

---
+++ packages/client-app-greeter/src/views/Home/index.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  div.home
+    img(
+      alt="Vue logo"
+      src="../../assets/logo.png"
+    )
+    HelloWorld(
+      v-show="message"
+      :msg="message"
+    )
+</template>
+
+<script src="./main.js"></script>
