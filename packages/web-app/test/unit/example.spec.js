e6cd0e0b19529bbf89f126db9d0f5c91d42343a5
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2020-01-07 03:52:47 +0400

    test(web-app): update with new components
    Drop stale tests, add
    
    - MessageList
    - MessageForm

--- packages/web-app/test/unit/example.spec.js
+++ packages/web-app/test/unit/example.spec.js
@@ -1,10 +1,25 @@
 import test from 'ava'
 
-import { shallowMount } from '@vue/test-utils'
+import { createLocalVue, shallowMount } from '@vue/test-utils'
 
-import HelloWorld from '@/components/HelloWorld'
+import Element from 'element-ui'
 
-test('HelloWorld should render', t => {
-  const wrapper = shallowMount(HelloWorld)
+import MessageList from '@/components/MessageList'
+import MessageForm from '@/components/MessageForm'
+
+test.before(t => {
+  const localVue = createLocalVue()
+  localVue.use(Element)
+
+  t.context = { localVue }
+})
+
+test('MessageList should render', t => {
+  const wrapper = shallowMount(MessageList, t.context)
+  t.is(wrapper.constructor.name, 'VueWrapper')
+})
+
+test('MessageForm should render', t => {
+  const wrapper = shallowMount(MessageForm, t.context)
   t.is(wrapper.constructor.name, 'VueWrapper')
 })
